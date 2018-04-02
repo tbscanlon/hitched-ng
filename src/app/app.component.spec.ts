0a2bf7b470b4e28949d27bddc7dcbb02a48d63d0
Tom Scanlon <thomas.scanlon@outlook.com>
2018-04-02 14:11:26 +0100

    Fix app component unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,39 @@
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+
 describe("AppComponent", () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: any;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     }).compileComponents();
-  }));
-  it("should create the app", async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("app");
-  }));
-  it("should render title in a h1 tag", async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture = TestBed.createComponent(AppComponent);
+    app     = fixture.debugElement.componentInstance;
+
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain("Welcome to app!");
   }));
+
+  describe("Initialisation", () => {
+    it("should create the app", async(() => {
+      expect(app).toBeTruthy();
+    }));
+
+    it(`should have as title 'app'`, async(() => {
+      expect(app.title).toEqual("app");
+    }));
+
+    it("has a navigation menu", async(() => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector("app-nav-menu")).toBeTruthy();
+    }));
+  });
 });
