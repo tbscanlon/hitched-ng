d07a92d467334df4b042aa7264c4e377063b14f7
tbscanlon <thomas.scanlon@outlook.com>
2018-04-14 17:15:00 +0100

    hero banner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppComponent } from "./app.component";
 import { NavMenuComponent } from "./components/nav-menu/nav-menu.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    FooterComponent
+    FooterComponent,
+    HeroBannerComponent
   ],
   imports: [
     BrowserModule,
